a7cdf776e5c6bd4fa0f17c9e6d10082e41330e6a
Risto Stevcev <risto1@gmail.com>
2017-09-26 07:42:31 +0200

    Update dependencies and syntax

--- src/Data/Undefinable.purs
+++ src/Data/Undefinable.purs
@@ -17,29 +17,29 @@ import Data.Maybe (Maybe(..), maybe)
 -- |
 -- | This type constructor may be useful when interoperating with JavaScript functions
 -- | which accept or return undefined values.
-foreign import data Undefinable :: * -> *
+foreign import data Undefinable ∷ Type → Type
 
 -- | The undefined value.
-foreign import undefined :: forall a. Undefinable a
+foreign import undefined ∷ ∀ a. Undefinable a
 
-foreign import undefinable :: forall a r. Fn3 (Undefinable a) r (a -> r) r
+foreign import undefinable ∷ ∀ a r. Fn3 (Undefinable a) r (a → r) r
 
 -- | Wrap a non-undefined value.
-foreign import notUndefined :: forall a. a -> Undefinable a
+foreign import notUndefined ∷ ∀ a. a → Undefinable a
 
 -- | Takes `Nothing` to `undefined`, and `Just a` to `a`.
-toUndefinable :: forall a. Maybe a -> Undefinable a
+toUndefinable ∷ ∀ a. Maybe a → Undefinable a
 toUndefinable = maybe undefined notUndefined
 
 -- | Represent `undefined` using `Maybe a` as `Nothing`.
-toMaybe :: forall a. Undefinable a -> Maybe a
+toMaybe ∷ ∀ a. Undefinable a → Maybe a
 toMaybe n = runFn3 undefinable n Nothing Just
 
-instance showUndefinable :: Show a => Show (Undefinable a) where
+instance showUndefinable ∷ Show a ⇒ Show (Undefinable a) where
   show = maybe "undefined" show <<< toMaybe
 
-instance eqUndefinable :: (Eq a) => Eq (Undefinable a) where
+instance eqUndefinable ∷ (Eq a) ⇒ Eq (Undefinable a) where
   eq = eq `on` toMaybe
 
-instance ordUndefinable :: (Ord a) => Ord (Undefinable a) where
+instance ordUndefinable ∷ (Ord a) ⇒ Ord (Undefinable a) where
   compare = compare `on` toMaybe
